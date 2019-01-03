50605032cac0ceecd0e3b2b8e9c296e0a29b233b
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-01-03 12:17:29 +0100

    added multi-point linear extrapolation

---
+++ polyfill/filter.js
@@ -0,0 +1,32 @@
+if (!Array.prototype.filter){
+  Array.prototype.filter = function(func, thisArg) {
+    'use strict';
+    if ( ! ((typeof func === 'Function' || typeof func === 'function') && this) )
+        throw new TypeError();
+
+    var len = this.length >>> 0,
+        res = new Array(len),
+        t = this, c = 0, i = -1;
+    if (thisArg === undefined){
+      while (++i !== len){
+        if (i in this){
+          if (func(t[i], i, t)){
+            res[c++] = t[i];
+          }
+        }
+      }
+    }
+    else{
+      while (++i !== len){
+        if (i in this){
+          if (func.call(thisArg, t[i], i, t)){
+            res[c++] = t[i];
+          }
+        }
+      }
+    }
+
+    res.length = c; 
+    return res;
+  };
+}
--- test/linear_ext.js
+++ test/linear_ext.js
@@ -1,31 +1,59 @@
 require('chai').should();
+const expect = require('chai').expect;
 
 const linear_ext = require('../lib/linear_ext');
 
 
-describe('linear_ext()', () => {
+describe('linear_ext.single()', () => {
   it('should do proper linear extrapolation between two given points.', () => {
-    var ext = linear_ext([0, 2], [1, 1]);
+    let ext = linear_ext.single([0, 2], [1, 1]);
     ext(0.5).should.equal(1.5);
     ext(0.75).should.equal(1.25);
   });
 
   it('for ([a0, a1], b) and k <= a0, it should give a1', () => {
-    var ext = linear_ext([0, 2], [1, 1]);
+    let ext = linear_ext.single([0, 2], [1, 1]);
     ext(0).should.equal(2);
     ext(-1).should.equal(2);
   });
 
   it('for (a, [b0, b1]) and k >= b0, it should give b1', () => {
-    var ext = linear_ext([0, 2], [1, 1]);
+    let ext = linear_ext.single([0, 2], [1, 1]);
     ext(1).should.equal(1);
     ext(2).should.equal(1);
   });
 
   it('for ([x, y], [x, z]) should give y for every input.', () => {
-    var ext = linear_ext([0, 2], [0, 1]);
+    let ext = linear_ext.single([0, 2], [0, 1]);
     ext(0).should.equal(2);
     ext(1).should.equal(2);
     ext(-1).should.equal(2);
+  });
+
+  it('should give out the value of one point if the other is not given.', () => {
+    linear_ext.single([0, 2])(128).should.equal(2);
+    linear_ext.single(undefined, [1, 1])(-128).should.equal(1);
+  });
+
+  it('should throw proper error if invalid input is given.', () => {
+    expect(() => linear_ext.single()).to.throw;
+    expect(() => linear_ext.single(2, 'hellow')).to.throw;
+    expect(() => linear_ext.single([0])).to.throw;
+  })
+});
+
+describe('linear_ext.multi()', () => {
+  it('should do proper linear extrapolation for multiple points.', () => {
+    let ext = linear_ext.multi([[0, 0], [1, 1], [2, 0]]);
+    ext(0.5).should.equal(0.5);
+    ext(1.5).should.equal(0.5);
+  });
+
+  it('should return the min(max) value for input lesser(greater) than the min(max) point.', () => {
+    let ext = linear_ext.multi([[0, 0], [1, 1], [2, 0]]);
+    ext(0).should.equal(0);
+    ext(2).should.equal(0);
+    ext(-1).should.equal(0);
+    ext(3).should.equal(0);
   });
 });
