96693d494ec3b87d2939f938db1aa805e580aac7
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-01-03 12:20:53 +0100

    fixed an error in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import linear_ext from './lib/linear_ext';
 function gleiten() {}
 
 gleiten.prototype.init = function() {
-  console.log(linear_ext([0, 0], [1, 1])(0.2));
+  console.log(linear_ext.single([0, 0], [1, 1])(0.2));
 }
 
 export default new gleiten();
